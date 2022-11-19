91c1f0cece0ebb84e91f50967c395534258cb351
renan <naneraruom@gmail.com>
2022-11-19 08:56:36 -0300

    remove ts.ts

--- ts.ts
+++
@@ -1,9 +0,0 @@
-import { CustomError } from "./src/helper/custom_error"
-
-try {
-    throw new CustomError('odfjop', 400)
-} catch (error) {
-    console.log((error as Error).message)
-}
-
-console.log('execultou')
